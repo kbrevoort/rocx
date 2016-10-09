11b568d939cac1657fbe48e5fa52dd462c8902ec
kbrevoort <ken@brevoort.com>
2016-10-09 12:23:33 -0400

    Added vignette on Using rocx

---
+++ inst/doc/using_rocx.R
@@ -0,0 +1,16 @@
+## ----echo = FALSE, fig.width = 4-----------------------------------------
+if ('png' %in% rownames(installed.packages())) {
+  img <- png::readPNG('sample_rocx_output.png')
+  grid::grid.raster(img)
+} else {
+  print('![](sample_rocx_output.png)')
+}
+
+## ----echo = FALSE, fig.width = 4-----------------------------------------
+if ('png' %in% rownames(installed.packages())) {
+  img <- png::readPNG('sample_rocx_template.png')
+  grid::grid.raster(img)
+} else {
+  print('![](sample_rocx_template.png)')
+}
+
